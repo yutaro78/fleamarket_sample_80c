f11886630eb9487476d11fa6ba8237f9199045fc
yutaro78 <tarokun0708@gmail.com>
2020-10-21 00:10:26 +0900

    first

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   #get 'tops/index'
-  root 'tops#index'
+  root 'items#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: :show
+  resources :items, only: :new
 end
