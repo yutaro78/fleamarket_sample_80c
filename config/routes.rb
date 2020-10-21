2ee76855ad6bd06b0ca3162796e5d27e8a377a15
naokimatsushita0315 <7oki0315@gmail.com>
2020-10-22 01:15:41 +0900

    parchasesコントローラー、ビュー作成。ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   root 'tops#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: :show
+  resources :purchases, only: :index
 end
