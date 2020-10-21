b3d63be28e1a687bc3481ea541260342fcfd1035
masa-endo-1111 <madja1111@icloud.com>
2020-10-22 04:26:27 +0900

    カテゴリモデル作成。マイグレーションファイル編集。ルーティング設定。
    ①カテゴリモデル作成。
    ②マイグレーションファイル編集。
    （item、categories、images）
    ③ファイル名時系列部分変更。（references型による読み込みエラー防止の為）
    ③トップページルーティング設定。

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  #get 'tops/index'
-  root 'tops#index'
+  root 'items#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: :show
   resources :items, only: [:index, :new, :create]
--- db/migrate/20201021183260_create_images.rb
+++ db/migrate/20201021183260_create_images.rb
@@ -1,6 +1,8 @@
 class CreateImages < ActiveRecord::Migration[6.0]
   def change
     create_table :images do |t|
+      t.string :url,      null: false
+      t.references :item, null: false, foreign_key: true
 
       t.timestamps
     end
