390c16e72c8af4ee8e928669a3e04e6961b8d3ec
tom20180126 <68907274+tom20180126@users.noreply.github.com>
2020-10-28 23:45:39 +0900

    Added User Registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
+  before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  private
+  protected
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
