c79f21cfcd2bd08a02fa74a5d143851a7c971f33
masa-endo-1111 <madja1111@icloud.com>
2020-11-03 02:24:57 +0900

    商品編集画面、複数画像投稿（仮）、商品削除機能実装
    ①商品出品ページをパーシャルへ→商品編集ページでも使用
    ②商品詳細ページに商品削除ボタン追加、モデルコントローラーの編集
    ③複数枚画像投稿できるように実装（現状複数枚画像選択時のビュー崩れ有り）
    
    ※複数枚投稿して、出品、削除、編集まで保存できているのを確認

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,38 +1,56 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
-    @items = Item.includes(:images).order('created_at DESC')
   end
-
+  
   def new
     @item = Item.new
     @item.images.new
     @category_parents = Category.where(ancestry: nil)
   end
-
+  
   def create
     @item = Item.new(item_params)
     # binding.pry
-    @item.save
-    redirect_to new_item_path
+    if @item.save
+      redirect_to new_item_path
+      # redirect_to root_path
+    else
+      render new_item_path
+      # render :new
+    end
   end
-
+  
   def show
+    @items = Item.includes(:images).order('created_at DESC')
   end
 
   def edit
+    @category_parents = Category.where(ancestry: nil)
   end
 
   def update
+    if @item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @item.destroy
+    redirect_to root_path
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:name, :introduction, :price, :condition, :size_id, :category_id, :brand, :prefecture_id, :shipping_cost_id, :shipping_days_id, images_attributes: [:url]).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :introduction, :price, :condition, :size_id, :category_id, :brand, :prefecture_id, :shipping_cost_id, :shipping_days_id, images_attributes: [:url, :_destroy, :id]).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 
 end
