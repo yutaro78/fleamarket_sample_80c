22de77f338e9c129efe8f3b7b3d6cfa841d25a41
tom20180126 <68907274+tom20180126@users.noreply.github.com>
2020-11-03 22:13:34 +0900

    Update User Models

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,12 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   VALID_PASSWPRD_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :password, format: { with: VALID_PASSWPRD_REGEX }
+  validates :first_name, format: { with: /\A([ぁ-んァ-ン一-龥]|ー)+\z/ }, presence: true
+  validates :last_name, format: { with: /\A([ぁ-んァ-ン一-龥]|ー)+\z/ }, presence: true
+  validates :firstname_kana, format: { with: /\A([ァ-ン]|ー)+\z/ }, presence: true
+  validates :lastname_kana, format: { with: /\A([ァ-ン]|ー)+\z/ }, presence: true
+  validates :birth, presence: true
+  validates :nickname,  presence: true
   
-  validates :nickname, :firstname, :lastname, :firstname_kana, :lastname_kana, :birth, presence: true
   has_one :address
 end
