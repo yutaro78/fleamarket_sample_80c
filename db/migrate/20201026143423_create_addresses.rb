2a8961d3d8e37a90f149676fc75cbb1039450353
yutaro78 <tarokun0708@gmail.com>
2020-11-06 11:34:26 +0900

    first
    Detail page

--- db/migrate/20201026143423_create_addresses.rb
+++ db/migrate/20201026143423_create_addresses.rb
@@ -2,7 +2,7 @@ class CreateAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :addresses do |t|
       t.string :post_code,        null: false
-      t.integer :prefecture_code, null: false
+      t.integer :prefecture_id,   null: false
       t.string :city,             null: false
       t.string :house_number,     null: false
       t.string :building_name
