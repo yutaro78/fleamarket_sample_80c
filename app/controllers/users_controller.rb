7e93753a4a20a63f34c5974b1e7bfcd80725c505
tom20180126 <68907274+tom20180126@users.noreply.github.com>
2020-11-06 23:10:23 +0900

    Update User model and controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,29 +1,6 @@
 class UsersController < ApplicationController
-  before_action :move_to_index, except: [:index, :show]
-
-  def new
-  end
-  
-  def create
-    if current_user.create(user_params)
-      redirect_to root_path
-    else
-      render :create
-    end
-  end
 
   def show
   end
 
-  private
-
-  def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
-    end
-  end
-
-  def user_params
-    params.require(:user).permit(:name, :email)
-  end
 end
