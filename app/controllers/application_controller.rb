5e6c95080e2b58dae11617f04acdb57212b57112
tom20180126 <68907274+tom20180126@users.noreply.github.com>
2020-11-07 21:10:47 +0900

    Modified the Application Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
-  before_action :authenticate_user!, only: [:show]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
