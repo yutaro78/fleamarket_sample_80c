0f6e66193962330ae7d41c7aa58a87a35cf28535
masa-endo-1111 <madja1111@icloud.com>
2020-11-11 23:06:27 +0900

    画像投稿機能修正①
    出品、編集時の画像投稿機能が実装途中であった為、ビューとscssも含め大幅修正（完全ではない）。以下詳細
    『変更前の問題点』
    ①画像選択時のビュー崩れあり
    ②画像を削除時に新たに画像のアップロード選択が開かれる。また１枚目の画像表示前に削除ボタンを押すと削除ボタンが消え画像が削除できなくなる。
    ③２枚目以降にファイルの選択エリアをクリックするとファイルの変更となってしまう。
    ④編集時にプレビュー表示されるが、そのまま削除ボタンを押しても画像が削除されない。
    
    『変更後』
    変更前の問題点は全て解消したが、５枚アップロードした場合のみ編集画面で６枚目の画像がアップロードできる（最終的に5枚＋5枚の10枚アップ予定）。

--- app/assets/javascripts/item-images.js
+++ app/assets/javascripts/item-images.js
@@ -1,20 +1,36 @@
 $(document).on('turbolinks:load', ()=> {
   // 画像用のinputを生成する関数
-  const buildFileField = (index)=> {
-    const html = `<div data-index="${index}" class="js-file_group">
+  const buildFileField = (num)=> {
+    const html = `<div data-index="${num}" class="js-file_group">
                     <input class="js-file" type="file"
-                    name="item[images_attributes][${index}][url]"
-                    id="item_images_attributes_${index}_url"><br>
-                    <div class="js-remove">削除</div>
+                    name="item[images_attributes][${num}][url]"
+                    id="item_images_attributes_${num}_url"><br>
+                  </div>`;
+    return html;
+  }
+  // プレビュー用のimgタグを生成する関数
+  const buildImg = (index, url)=> {
+    const html = `<div class="preview">
+                    <label for="item_images_attributes_${index}_url">
+                    <img src="${url}" width="110px" height="110px" data-index="${index}"></label>
+                    <div class="js-remove" >削除</div>
                   </div>`;
     return html;
   }
 
-  // プレビュー用のimgタグを生成する関数
-  const buildImg = (index, url)=> {
-    const html = `<img data-index="${index}" src="${url}" width="124px" height="124px">`;
+  const buildlabel = (num)=> {
+    const html = `item_images_attributes_${num + 1}_url`;
     return html;
   }
+  const buildlabel2 = (num)=> {
+    const html = `item_images_attributes_${num}_url`;
+    return html;
+  }
+  const buildeditlabel = (num)=> {
+    const html = `item_images_attributes_${num}_url`;
+    return html;
+  }
+  
 
   // file_fieldのnameに動的なindexをつける為の配列
   let fileIndex = [1,2,3,4,5,6,7,8,9,10];
@@ -24,36 +40,75 @@ $(document).on('turbolinks:load', ()=> {
 
   $('.hidden-destroy').hide();
 
-  $('#tab__box').on('change', '.js-file', function(e) {
+  // 画像選択でイベント発火
+  $('#image-box').on('change', '.js-file', function(e) {
     const targetIndex = $(this).parent().data('index');
     // ファイルのブラウザ上でのURLを取得する
     const file = e.target.files[0];
     const blobUrl = window.URL.createObjectURL(file);
+    lastIndex = $('.js-file_group:last').data('index');
 
+    // 要素を取得→プロパティを取得
+    const element = document.getElementById("label_image");
+    element.htmlFor;
+    
     // 該当indexを持つimgがあれば取得して変数imgに入れる(画像変更の処理)
     if (img = $(`img[data-index="${targetIndex}"]`)[0]) {
       img.setAttribute('src', blobUrl);
     } else {  // 新規画像追加の処理
-      $('#previews').append(buildImg(targetIndex, blobUrl));
-      // fileIndexの先頭の数字を使ってinputを作る
-      $('#tab__box').append(buildFileField(fileIndex[0]));
-      fileIndex.shift();
-      // 末尾の数に1足した数を追加する
-      fileIndex.push(fileIndex[fileIndex.length - 1] + 1);
+      $('#image-box').append(buildImg(targetIndex, blobUrl));
+
+      const previewcount = $('.preview').length;
+      const previewscount = $('.previews').length;
+     
+        // fileIndexの先頭の数字を使ってinputを作る
+        $('#image-box').append(buildFileField(fileIndex[0]));
+        fileIndex.shift();
+        // 末尾の数に1足した数を追加する
+        fileIndex.push(fileIndex[fileIndex.length - 1] + 1);
+        
+        element.htmlFor = (buildlabel(lastIndex));
+
+        // クリックエリアを非表示
+        if (previewcount > 4) {
+        $('#label_image').hide();
+      }
+        if (previewcount + previewscount > 4) {
+        $('#label_image').hide();
+      }
     }
   });
 
-  $('#tab__box').on('click', '.js-remove', function() {
-    const targetIndex = $(this).parent().data('index');
+  // 削除ボタンを押すとイベント発火
+  $('#image-box').on('click', '.js-remove', function() {
+    const targetIndex = $(this).parent().prev().data('index');
+
+    $(`#item_images_attributes_${targetIndex}_url`).remove()
+
     // 該当indexを振られているチェックボックスを取得する
     const hiddenCheck = $(`input[data-index="${targetIndex}"].hidden-destroy`);
     // もしチェックボックスが存在すればチェックを入れる
     if (hiddenCheck) hiddenCheck.prop('checked', true);
 
-    $(this).parent().remove();
-    $(`img[data-index="${targetIndex}"]`).remove();
+    lastIndex = $('.js-file_group:last').data('index');
 
-    // 画像入力欄が0個にならないようにしておく
-    if ($('.js-file').length == 0) $('#tab__box').append(buildFileField(fileIndex[0]));
+
+    $(this).parent().remove();
+
+    // クリックエリアを表示
+    const previewcount = $('.preview').length;
+      if (previewcount < 5) {
+        $('#label_image').show();
+      }
+
+      // 画像入力欄が0個にならないようにしておく
+    if ($('.js-file').length == 0) $('#image-box').append(buildFileField(fileIndex[0]));
   });
+
+  // 要素を取得→プロパティを取得→プロパティに代入
+  // if ($('.js-file').length > 1) {
+  // const element = document.getElementById("label_image");
+  // element.htmlFor;
+  // element.htmlFor = (buildlabel2(lastIndex));
+  // }
 });
