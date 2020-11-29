86e9761a281a05ad1dca74f3a3aad4d55b2b0d59
tom20180126 <68907274+tom20180126@users.noreply.github.com>
2020-11-29 22:38:48 +0900

    Add flash-message

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -40,7 +40,8 @@ class Users::RegistrationsController < Devise::RegistrationsController
     session["devise.regist_data"]["user"].clear
     # ログインすること
     sign_in(:user, @user)
-    render :create_address
+    # render :create_address
+    redirect_to root_path, notice: "登録が完了しました"
   end
 
   # GET /resource/edit
