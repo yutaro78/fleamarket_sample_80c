85a052eb24ba78913433eca9f1c7844b61967a35
tom20180126 <68907274+tom20180126@users.noreply.github.com>
2020-12-12 16:01:02 +0900

    Changed Category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,10 +1,16 @@
 class CategoriesController < ApplicationController
-  before_action :set_parents, only: [:new, :create, :edit, :index]
-  before_action :set_category, only: :show
+  before_action :set_parents, only: [:new, :create, :edit, :index, :show]
+  before_action :set_category, only: [:show]
+
+  def index
+    @category_parents = Category.where(ancestry: nil)
+  end
 
   def show
     @items = @category.set_items
-    @items = @items.where(buyer_id: nil).order("created_at DESC").page(params[:page]).per(9)
+    # @items = @items.where(buyer_id: nil).order("created_at DESC")
+    @items = Item.includes(:images).order('created_at DESC')
+    @items = Item.order("id DESC").limit(3)
   end
 
   def search_category
@@ -22,10 +28,19 @@ class CategoriesController < ApplicationController
     end
   end
 
+  def set_parents
+    @category_parents = Category.where(ancestry: nil)
+  end
+
   private
 
   def set_category
     @category = Category.find(params[:id])
+    if @category.has_children?
+      @category_links = @category.children
+    else
+      @category_links = @category.siblings
+    end
   end
 
 end
