6f946680c6e7e37686ede3140d8b787badc36f9c
naokimatsushita0315 <7oki0315@gmail.com>
2020-12-15 12:58:12 +0900

    質問、コメント実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,13 @@
 class CommentsController < ApplicationController
   def create
     comment = Comment.create(comment_params)
-    redirect_to "/item/#{comment.item.id}" 
+    redirect_to "/items/#{comment.item.id}" 
   end
 
   private
   def comment_params
     params.require(:comment).permit(:comment).merge(user_id: current_user.id,item_id: params[:item_id] )
+
   end
 end
 
